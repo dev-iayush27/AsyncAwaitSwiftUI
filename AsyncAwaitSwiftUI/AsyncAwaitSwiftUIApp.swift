bd9d734ec858812c0b75f37ad26873dd0c717601
Ayush Gupta <dev.iayush27@gmail.com>
2023-10-26 18:49:32 +0530

    Project initiated

---
+++ AsyncAwaitSwiftUI/AsyncAwaitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitSwiftUIApp.swift
+//  AsyncAwaitSwiftUI
+//
+//  Created by Ayush Gupta on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
