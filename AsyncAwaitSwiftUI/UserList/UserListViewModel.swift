36153bd1d92f759951a7a3559acbd4b67a8fa959
Ayush Gupta <dev.iayush27@gmail.com>
2023-10-28 11:43:44 +0530

    Added view model

--- AsyncAwaitSwiftUI/UserList/UserListViewModel.swift
+++ AsyncAwaitSwiftUI/UserList/UserListViewModel.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+@MainActor
+final class UserListViewModel: ObservableObject {
+    
+    @Published var users: [User]?
+    @Published var userError: UserError?
+    @Published var shouldShowAlert = false
+    @Published var isLoading = false
+    
+    func getUsers() async {
+        isLoading = true
+        Task {
+            do {
+                self.users = try await WebService.getUsersData()
+                self.isLoading = false
+            } catch(let error) {
+                userError = UserError.custom(error: error)
+                shouldShowAlert = true
+                isLoading = false
+            }
+        }
+    }
+}
