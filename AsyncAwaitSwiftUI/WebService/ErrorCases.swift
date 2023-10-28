d99dfaed7c1dce9effc426b396be443c24249704
Ayush Gupta <dev.iayush27@gmail.com>
2023-10-28 11:44:03 +0530

    Added error cases

--- AsyncAwaitSwiftUI/WebService/ErrorCases.swift
+++ AsyncAwaitSwiftUI/WebService/ErrorCases.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+enum UserError: LocalizedError {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case custom(error: Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "Invalid URL"
+            
+        case .invalidResponse:
+            return "Invalid Response"
+            
+        case .invalidData:
+            return "Invalid data"
+            
+        case .custom(let error):
+            return error.localizedDescription
+        }
+    }
+}
