cf808a1f67ad45496865564df875297b1347faa1
Ayush Gupta <dev.iayush27@gmail.com>
2023-10-28 11:44:52 +0530

    Added webservice for API call

--- AsyncAwaitSwiftUI/WebService/WebService.swift
+++ AsyncAwaitSwiftUI/WebService/WebService.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+final class WebService {
+    
+    static func getUsersData() async throws -> [User] {
+        let urlString = "https://api.github.com/users"
+        guard let url = URL(string: urlString) else {
+            throw UserError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw UserError.invalidResponse
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode([User].self, from: data)
+        } catch {
+            throw UserError.invalidData
+        }
+    }
+}
